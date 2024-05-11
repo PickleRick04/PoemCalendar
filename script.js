958b0d4da7055de66278fde5d8e3d8d22b680c62
Babineca Darja <dascha@MacBook-Air-von-Babineca.local>
2024-05-11 11:24:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     var password = prompt("Wie nenne ich Frau Deiss in Kleinbuchstaben:", "");
     if(password.toLowerCase === "hanchkik" && password != "hanchkik"){
-        var password = prompt("In Kleinbuchstaben du Depp:", "");
-        if (password === "hanchkik") {
+        window.onload = function() {
+            var password = prompt("In Kleinbuchstaben du Depp:", "");
+            if (password === "hanchkik") {
             document.getElementById('content').style.display = 'block';
+            }
         }
     }
     if (password === "hanchkik") {
