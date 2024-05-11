ba439c55a0c20b8b951bac4e9a631eee41fa5766
Babineca Darja <dascha@MacBook-Air-von-Babineca.local>
2024-05-11 11:16:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
-    var password = prompt("Please enter the password to view this page:", "");
-    if (password === "enter") {
+    var password = prompt("Wie nenne ich Frau Deiss in Kleinbuchstaben:", "");
+    if(password.toLowerCase === "hanchkik" && password != "hanchkik"){
+        var password = prompt("In Kleinbuchstaben du Depp:", "");
+    }
+    if (password === "hanchkik") {
         document.getElementById('content').style.display = 'block';
     } else {
         alert("Incorrect password. You cannot access the content.");
